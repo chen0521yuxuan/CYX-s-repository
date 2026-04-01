d8104412ffff37af02192ae1e5396eb5617b6fa1
Aaron Chen <chen0521yuxuan@qq.com>
2026-04-01 23:41:35 +0800

    commit Address and Student javafile

--- src/main/java/com/sharelight/assignment_01/Address.java
+++ src/main/java/com/sharelight/assignment_01/Address.java
@@ -1,4 +1,14 @@
 package com.sharelight.assignment_01;
 
 public class Address {
+    private String city;
+
+    // Getter and Setter
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
--- src/main/java/com/sharelight/assignment_01/Student.java
+++ src/main/java/com/sharelight/assignment_01/Student.java
@@ -1,4 +1,29 @@
 package com.sharelight.assignment_01;
 
 public class Student {
+    private String name;
+    private Address address;
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    // 打印信息的方法
+    public void displayInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("City: " + (address != null ? address.getCity() : "null"));
+    }
 }
