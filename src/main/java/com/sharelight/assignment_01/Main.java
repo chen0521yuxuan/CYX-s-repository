1baa8c2099278db4eb60637efec2538b731ee2d1
Aaron Chen <chen0521yuxuan@qq.com>
2026-04-01 23:42:17 +0800

    commit Main

--- src/main/java/com/sharelight/assignment_01/Main.java
+++ src/main/java/com/sharelight/assignment_01/Main.java
@@ -3,11 +3,19 @@ package com.sharelight.assignment_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        // 加载 Spring 配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+        // 获取 Student bean
+        Student student = (Student) context.getBean("student");
 
+        // 调用方法输出信息
+        student.displayInfo();
+    }
 }
