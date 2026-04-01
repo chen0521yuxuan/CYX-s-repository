26be942d6a684a33f5aa8453a321c53d96f4884c
Aaron Chen <chen0521yuxuan@qq.com>
2026-04-01 23:28:38 +0800

    init commit

---
+++ src/main/java/com/sharelight/assignment_01/Assignment01Application.java
@@ -0,0 +1,13 @@
+package com.sharelight.assignment_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharelight/assignment_01/Assignment01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharelight.assignment_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
